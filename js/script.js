de19bd7164f4e88851c07e4ec1d3a326fb9055cd
seiyf05 <seiyf05@gmail.com>
2026-06-19 14:53:55 +0300

    2

--- js/script.js
+++ js/script.js
@@ -7,16 +7,59 @@ const modalDescription = document.getElementById('modalDescription');
 
 const closeBtn = document.querySelector('.close');
 
+let currentImages = [];
+let currentIndex = 0;
+
+const modalPrice = document.getElementById('modalPrice');
+
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+
 cards.forEach(card => {
 
     card.addEventListener('click', () => {
 
-        modalImage.src = card.dataset.image;
+        currentImages = card.dataset.images.split(',');
+
+        currentIndex = 0;
+
+        modalImage.src = currentImages[currentIndex];
+
         modalTitle.textContent = card.dataset.title;
         modalDescription.textContent = card.dataset.description;
+        modalPrice.textContent = card.dataset.price;
+
+        prevBtn.style.display =
+            currentImages.length > 1 ? 'block' : 'none';
+
+        nextBtn.style.display =
+            currentImages.length > 1 ? 'block' : 'none';
 
         modal.style.display = 'flex';
     });
+
+});
+prevBtn.addEventListener('click', () => {
+
+    currentIndex--;
+
+    if (currentIndex < 0) {
+        currentIndex = currentImages.length - 1;
+    }
+
+    modalImage.src = currentImages[currentIndex];
+
+});
+
+nextBtn.addEventListener('click', () => {
+
+    currentIndex++;
+
+    if (currentIndex >= currentImages.length) {
+        currentIndex = 0;
+    }
+
+    modalImage.src = currentImages[currentIndex];
 
 });
 
